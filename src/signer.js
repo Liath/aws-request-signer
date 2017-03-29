314a7f9b1e95924fafa209c6d4f3def8972268f9
John Jones <john.jones@unifilabs.com>
2017-03-29 16:51:28 -0700

    Handle slashes in query string

--- src/signer.js
+++ src/signer.js
@@ -277,7 +277,7 @@ function getCanonicalQueryString(url) {
   for (var i=0; i<params.length; i++) {
 	  if (params[i].substr(0,1) === '?')
         params[i] = params[i].substr(1, params[i].length-1);
-      params[i] = params[i].split('=').map(decodeURIComponent).map(uriEncode).join('=');
+      params[i] = params[i].split('=').map(decodeURIComponent).map(uriEncodeSlash).join('=');
   }
 
   var sortedParams = params.sort();
@@ -337,13 +337,16 @@ function getHashedPayload(request) {
 function log(msg) {
   console.log( msg);
 }
-function uriEncode(input) {
+function uriEncodeSlash(input) {
+	return uriEncode(input, true)
+}
+function uriEncode(input, slash) {
   var ch;
   var i;
   var output = '';
   for (i = 0; i < input.length; i++) {
     ch = input[i];
-    if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9') || ch === '_' || ch === '-' || ch === '~' || ch === '.' || ch === '/') {
+    if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9') || ch === '_' || ch === '-' || ch === '~' || ch === '.' || (!slash && ch === '/')) {
       output += ch;
     } else {
       output += `%${ch.charCodeAt(0).toString(16).toUpperCase()}`;
